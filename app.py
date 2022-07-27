57a90ad9d8b6b5f7266ee6daa1656aef86786c30
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-07-26 21:21:33 -0300

    tenta consertar problema acoes estrategicas

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.write("Clique nas características abaixo e veja quais ações estratégicas,
 acoes = []
 
 if st.checkbox('Municípios com população vivendo em comunidades ribeirinhas'):
-    acoes.extend(['Informatiza APS','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Unidade Básica de Saúde Fluvial (UBSF)','Equipes de Saúde da Família Ribeirinha (eSFR)','Micropista']) 
-if st.checkbox('Até duas equipes, sem profissionais adicionais (além da equipe mínima), sem especificidadese e sem cirurgião dentista.'):
+    acoes.extend(['Informatiza APS','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Unidade Básica de Saúde Fluvial (UBSF)','Equipes de Saúde da Família Ribeirinha (eSFR)','Microscopista']) 
+if st.checkbox('Até duas equipes, sem profissionais adicionais (além da equipe mínima), sem especificidades e sem cirurgião dentista.'):
     acoes.extend(['Informatiza APS','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)']) 
 if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), sem especificidades'):
     acoes.extend(['Informatiza APS','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Equipes de Saúde Bucal','Unidade Odontológica Móvel (UOM)','Laboratório Regional de Prótese Dentária (LRPD)','Programa Academia da Saúde','Equipe de Consultório na Rua (eCR)'])
@@ -17,11 +17,11 @@ if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equ
     acoes.extend(['Informatiza APS','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Equipes de Saúde Bucal','Unidade Odontológica Móvel (UOM)','Laboratório Regional de Prótese Dentária (LRPD)','Programa Academia da Saúde','Equipe de Consultório na Rua (eCR)', 'Centro de Especialidades Odontológicas (CEO)'])
 if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), com especfificidades (sistema prisional adulto e jovem) e com profissionais residentes nas equipes'):
     acoes.extend(['Formação Profissional','Informatiza APS','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Equipes de Saúde Bucal','Unidade Odontológica Móvel (UOM)','Laboratório Regional de Prótese Dentária (LRPD)','Programa Academia da Saúde','Equipe de Consultório na Rua (eCR)', 'Centro de Especialidades Odontológicas (CEO)', 'Equipe de Atenção Básica Prisional (eABP)'])
-if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), possibilidade de equipes com funcionamento de pelo menos 60h semanais, sem especificades'):
+if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), possibilidade de equipes com funcionamento de pelo menos 60h semanais, sem especificidades'):
     acoes.extend(['Informatiza APS','Programa Saúde na Hora','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Equipes de Saúde Bucal','Unidade Odontológica Móvel (UOM)','Laboratório Regional de Prótese Dentária (LRPD)','Programa Academia da Saúde','Equipe de Consultório na Rua (eCR)', 'Centro de Especialidades Odontológicas (CEO)'])
-if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), possibilidade de equipes com funcionamento de pelo menos 60h semanais com especificades (sistema prisional adulto e jovem)'):
+if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), possibilidade de equipes com funcionamento de pelo menos 60h semanais com especificidades (sistema prisional adulto e jovem)'):
     acoes.extend(['Informatiza APS','Programa Saúde na Hora','Programa Saúde na Escola (PSE)','Estratégia de Agentes Comunitários de Saúde (ACS)','Equipes de Saúde Bucal','Unidade Odontológica Móvel (UOM)','Laboratório Regional de Prótese Dentária (LRPD)','Programa Academia da Saúde','Equipe de Consultório na Rua (eCR)', 'Centro de Especialidades Odontológicas (CEO)', 'Equipe de Atenção Básica Prisional (eABP)'])
-if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), possibilidade de equipes com funcionamento de pelo menos 60h semanais com especificades e com profissionais residentes nas equipes'):
+if st.checkbox('Duas ou mais equipes, com profissionais adicionais (além da equpe mínima), possibilidade de equipes com funcionamento de pelo menos 60h semanais com especificidades e com profissionais residentes nas equipes'):
     acoes.extend(['Formação Profissional','Programa Saúde na Hora','Informatiza APS','Residência Profissional','Equipes de Saúde Bucal','Unidade Odontológica Móvel (UOM)','Centro de Especialidades Odontológicas (CEO)','Laboratório Regional de Prótese Dentária (LRPD)','Programa Saúde na Escola (PSE)','Programa Academia da Saúde','Equipe de Consultório na Rua (eCR)','Equipe de Atenção Básica Prisional (eABP)','Estratégia de Agentes Comunitários de Saúde (ACS)','Atenção Integral à Saúde dos Adolescentes em Situação de Privação de Liberdade'])
 
 tabela_acoes = pd.read_csv("acoes.csv")
